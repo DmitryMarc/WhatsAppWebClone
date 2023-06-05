b538f56e138ce29f96e556ea38c9701ac110c069
DmitryMarch <dmitrymarchenkovforwork@gmail.com>
2023-06-05 19:02:27 +0300

    Fixed the popup.

--- src/entities/Menu/components/Menu.tsx
+++ src/entities/Menu/components/Menu.tsx
@@ -1,7 +1,7 @@
 import { FC, useState } from 'react'
 import menuIcon from '../../../shared/assets/img/menu.svg'
-import styles from './Menu.module.scss'
 import { Popup } from '../../../shared/ui/Popup/Popup'
+import styles from './Menu.module.scss'
 
 type MenuType = {
     setSelectedMenuItem: (selectedMenuItem: string) => void
@@ -10,9 +10,17 @@ type MenuType = {
 export const Menu: FC<MenuType> = ({setSelectedMenuItem}) => {
     const [isOpen, setIsOpen] = useState(false);
     return (
-        <button className={styles.menu} onClick={() => setIsOpen(!isOpen)}>
+        <button 
+            className={styles.menu} 
+            onClick={() => setIsOpen(!isOpen)}
+            onBlur={() => setIsOpen(!isOpen)}
+        >
             <img width='100%' src={menuIcon} alt='menu icon' />
-            <Popup list={['Выйти', 'Пункт 2', 'Пункт 3']} isOpen={isOpen} setSelectedMenuItem={setSelectedMenuItem} />
+            <Popup 
+                list={['Выйти', 'Пункт 2', 'Пункт 3']} 
+                isOpen={isOpen} 
+                setSelectedMenuItem={setSelectedMenuItem} 
+            />
         </button>
     )
 }
